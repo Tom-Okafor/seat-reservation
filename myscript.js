a1fa95c8b9a71c5aa6e04249c252220af1dff6b2
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-07-07 18:27:47 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -27,14 +27,43 @@
         ];
         let currentRow = 0;
         let currentNumberOfSeats = 15;
+        const COLUMN_ONE = 3;
+        const COLUMN_TWO = 9;
+        const COLUMN_THREE = 3;
+        const TOTAL_NUMBER_OF_SEATS = 300;
         function handleSeatCreation() {
             currentRow++;
+            labelRow(LEFT_SECTION);
+            for (let t = 0; t < COLUMN_ONE; t++) {
+                makeSeatsAndAttachThemInTheRightSection(LEFT_SECTION);
+            }
+            for (let t = 0; t < COLUMN_TWO; t++) {
+                makeSeatsAndAttachThemInTheRightSection(MIDDLE_SECTION);
+            }
+            for (let t = 0; t < COLUMN_THREE; t++) {
+                makeSeatsAndAttachThemInTheRightSection(RIGHT_SECTION);
+            }
+            labelRow(RIGHT_SECTION);
+            if (currentNumberOfSeats < TOTAL_NUMBER_OF_SEATS) {
+                handleSeatCreation();
+            }
+        }
+        function labelRow(section) {
             const NEW_ROW = document.createElement("div");
             NEW_ROW.className = "label";
             NEW_ROW.innerText = ROWS[currentRow];
-            LEFT_SECTION.appendChild(NEW_ROW);
+            section.appendChild(NEW_ROW);
         }
-        handleSeatCreation()
+
+        function makeSeatsAndAttachThemInTheRightSection(section) {
+            currentNumberOfSeats++;
+            const NEW_SEAT = document.createElement("div");
+            NEW_SEAT.id = `a${currentNumberOfSeats}`;
+            NEW_SEAT.innerText = currentNumberOfSeats;
+            section.appendChild(NEW_SEAT);
+        }
+
+        handleSeatCreation();
     }
     createAndAddSeats();
 })();
